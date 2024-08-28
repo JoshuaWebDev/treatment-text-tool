6b9b9c05c5d4d9a4d895ad133d4ae6ea7aa8adc8
Josué Barros da Silva <josue.barros1986@gmail.com>
2024-08-28 06:38:20 -0300

    save data as dictionary into items list

--- text-utilities-tools.py
+++ text-utilities-tools.py
@@ -3,26 +3,47 @@ import os
 def main():
     input_file = open('input/test.csv', 'r', encoding='utf-8')
     output_file = open('output/test.csv', 'w', encoding='utf-8')
+    
+    # define o campo que será tratado
+    target = 'CPF'
 
+    # a partir da primeira linha do arquivo, cria uma lista com o nome dos campos (atributos)
     first_line = input_file.readline()
     head = first_line.split(';')
-    first_line = head[0] + ";" + head[1] + ";" + head[2].strip()
+    head[-1] = head[-1][0:-1] # elimina a quebra de linha do último item da lista (\n)
 
     items = list()
-    items.append(first_line)
 
+    # adiciona cada um dos itens à lista items, de acordo com seu respectivo campo (atributo)
     for line in input_file:
         if line != first_line:
             temp = line.split(';')
-            cpf = temp[2].strip()
-            cpf = cpf[0:3] + ".***.***-" + cpf[-2:]
-            item = temp[0] + ";" + temp[1] + ";" +  cpf
-            items.append(item)
+            item = list()
+            # trata os dados, eliminando espaços ao redor e quebra de linha (\n)
+            # em seguida salva o item (tratado) na lista item
+            for t in temp:
+                item.append(t.strip())
+
+            dictio = {} # cria um dicionário vazio
+            for i in range(len(item)):
+                dictio[head[i]] = item[i]
+            items.append(dictio)
+            
+            #cpf = temp[2].strip()
+            #cpf = cpf[0:3] + ".***.***-" + cpf[-2:]
 
     input_file.close()
 
+    print(repr(items))
+
+    exit()
+
+    # pega o conteúdo da lista items e salva no arquivo, após serem tratados os dados
     for item in items:
-        output_file.write(item + "\n")
+        temp = ";".join(item)
+        output_file.write(temp + "\n")
+
+    output_file.close()
 
     print("Arquivo criado com sucesso em", output_file.name)
 
